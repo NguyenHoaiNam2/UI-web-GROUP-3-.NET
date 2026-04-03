bf0c4db2c58d8283597785878163445efe0d61c0
Pnix Jone <ha.truong.04071982@gmail.com>
2026-04-03 21:26:18 +0700

    global online

---
+++ src/app/services/signalr.ts
@@ -0,0 +1,17 @@
+// services/signalr.ts
+import * as signalR from "@microsoft/signalr";
+import { url } from "../../env";
+
+let connection: signalR.HubConnection | null = null;
+
+export const getSignalRConnection = () => {
+  if (!connection) {
+    connection = new signalR.HubConnectionBuilder()
+      .withUrl(url + "consultationHub", {
+        accessTokenFactory: () => localStorage.getItem("accessToken") || ""
+      })
+      .withAutomaticReconnect()
+      .build();
+  }
+  return connection;
+};
